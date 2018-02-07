ff4673fe3cbcabb9792f8a8985569a9a5c930dae
Pavel Vasilev <pvasilev@e3ag.ch>
2018-02-07 18:39:46 +0100

    new tests added, autogerenare report

--- src/test/java/io/qameta/junit5/AdvancedTest.java
+++ src/test/java/io/qameta/junit5/AdvancedTest.java
@@ -1,54 +1,53 @@
 package io.qameta.junit5;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Step;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
  * eroshenkoam
  * 24.10.17
  */
-public class SimpleTest {
+public class AdvancedTest {
 
     @Test
-    @DisplayName("Test number 1, critical")
-    @Feature("Some feature")
+    @DisplayName("Test number 13, critical")
+    @Feature("Some feature 324")
     @Severity(SeverityLevel.CRITICAL)
     public void testOutput() {
-        firstStep();
+        firstStep("12");
         secondStep();
         fourthStep();
     }
 
     @Test
-    @DisplayName("Test number 2, normal")
-    @Feature("Some feature 2")
+    @DisplayName("Test number 24, normal")
+    @Feature("Some feature 25")
     @Severity(SeverityLevel.NORMAL)
     public void testOutput_2() {
-        firstStep();
+        firstStep("12");
         secondStep();
-        thirdStep();
+        thirdStep("14");
     }
 
     @Step
-    public void firstStep() {
-//        assertEquals("12","14");
+    public void firstStep(String sa) {
+        assertEquals("12",sa);
     }
 
     @Step
     public void secondStep() {
-
     }
 
     @Step
-    public void thirdStep() {
-
+    public void thirdStep(String param) {
+        assertEquals("12", param);
     }
 
     @Step
